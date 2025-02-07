0af7e1dfc0409a32bba0c8a3b6eb10b9f0bbcca0
Jim Champagne <jim.chmpgn@gmail.com>
2025-02-07 19:44:02 +0100

    Add user route for auth

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -7,9 +7,9 @@ use App\Http\Controllers\TicketController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
 // Resources
 
